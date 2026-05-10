25d33ede298ddb979a6355e3f5a25708dd3a6931
Stephen <stephenmissah@outlook.com>
2026-05-10 21:55:21 +0100

    adjust cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 app.use(cors(
   {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://your-vercel-app.vercel.app"],
   }
 ));
 
