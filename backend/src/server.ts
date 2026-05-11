f168d88abf87b707f392656db8591a01e90a34a9
Stephen <stephenmissah@outlook.com>
2026-05-11 09:21:29 +0100

    cors fix.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://your-vercel-app.vercel.app"],
+    origin: ["http://localhost:5173", "https://spotify-react-ts-five.vercel.app"],
   }),
 );
 
